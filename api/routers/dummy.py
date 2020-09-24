48a96da8bbd89efeba9ac96122e6e44807bb8a79
floschne <florian.schneider.1992@gmx.de>
2020-09-24 16:48:05 +0200

    fixed bug in dummy prediction endpoint that lead to errors in generation of the api docs

--- api/routers/dummy.py
+++ api/routers/dummy.py
@@ -7,14 +7,15 @@ from logger import api_logger
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", tags=["dummy"])
 async def get_root():
+    api_logger.info("GET request on /")
     return "Hello world from Codebook Automation API!"
 
 
-@router.post("/info")
-async def get_info(req: PredictionRequest, response_model=PredictionResult):
-    api_logger.info(f"POST request on /info with %s" % req.json())
+@router.post("/predict", response_model=PredictionResult, tags=["dummy"])
+async def get_info(req: PredictionRequest):
+    api_logger.info(f"POST request on /predict with %s" % req.json())
     return PredictionResult(doc_id=req.doc.doc_id,
                             proj_id=req.doc.proj_id,
                             codebook_name=req.codebook.name,
